8cccbc843293c4d122b22b27339cf8c666e5f5b1
PeacemakerLM <louismukama@gmail.com>
2016-06-29 11:27:18 -0500

    Add files via upload

---
+++ Card Files/CardDriver.java
@@ -0,0 +1,20 @@
+//********************************************************************
+//  CardDriver.java       Author: Lewis and Loftus
+//
+//  Solution to Programming Project 7.7 
+//********************************************************************
+import java.util.ArrayList;
+
+public class CardDriver
+{
+   //-----------------------------------------------------------------
+   //  Creates a deck, shuffles the deck and deals the cards.
+   //-----------------------------------------------------------------
+   public static void main (String args[])
+   {
+     
+       
+       
+       
+   }
+}
